2de0dd3703b55be9fe20f63b74607067b03bfad7
Marco von Rosenberg <codingmarco@gmail.com>
2024-09-01 20:51:05 +0200

    huaweicreator.sh: Create padded ELF image that can be flashed to 0:APPSBL with flashrom

--- huaweicreator.sh
+++ huaweicreator.sh
@@ -7,6 +7,9 @@ then
 	echo "Usage: huaweicreator.sh <BOARDNAME>"
 	exit 1
 fi
+
+UBOOT_ELF="u-boot"
+UBOOT_ELF_FLASH="appsbl.bin"
 UBOOT_BIN="u-boot.bin"
 UBOOT_HUAWEI="uboot-${BOARDNAME}.bin"
 UIMAGE_OUT="uImage"
@@ -28,5 +31,13 @@ cat "$HUAWEI_DTB" "$HUAWEI_DTB" "$HUAWEI_DTB" "$HUAWEI_DTB" >> $UBOOT_HUAWEI
 rm -f "$HUAWEI_DTB"
 
 mkimage -A arm -C none -T kernel -a "$UBOOT_LOADADDR" -e "$UBOOT_ENTRYADDR" -d "$UBOOT_BIN" "$UIMAGE_OUT"
+
+# Strip symbols from u-boot ELF
+arm-openwrt-linux-strip -s "$UBOOT_ELF"
+
+cp "$UBOOT_ELF" "$UBOOT_ELF_FLASH"
+
+# Pad ELF to partition size of 1MB to make it flashable with flashrom
+dd if=/dev/zero of="$UBOOT_ELF_FLASH" bs=1 count=0 seek=1M
 
 echo "Done."
